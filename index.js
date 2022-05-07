b9900da45fd49a8325b97061e82744126b045499
Tohidul29 <tohidswfu@gmail.com>
2022-05-07 19:05:59 +0600

    post data added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -15,19 +15,42 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.nlpim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const productCollection = client.db('Apple-Mania').collection('product');
 
-        app.get('/product', async(req, res)=>{
+        //load all data:
+        app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
-    }   
-    finally{
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        //delete a single product:
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //send data to the backend:
+        app.post('/product', async(req, res)=>{
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
+    }
+    finally {
 
     }
 }
@@ -36,10 +59,10 @@ run().catch(console.dir);
 
 
 //routes:
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('running Apple-Mania server');
 });
 
-app.listen(port, ()=>{
-    console.log('Port no:',port);
+app.listen(port, () => {
+    console.log('Port no:', port);
 })
