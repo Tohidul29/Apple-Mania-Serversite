4692eb12427c7bcf96d41d1395b46ff43831ef64
Tohidul29 <tohidswfu@gmail.com>
2022-05-07 20:03:46 +0600

    added git ignore

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
             res.send(products);
         });
 
+        //find a single data 
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
