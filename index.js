a1da95a09cf1b85f8afb32611e3507d216a14593
Tohidul29 <tohidswfu@gmail.com>
2022-05-08 20:24:34 +0600

    code added for update single product

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function run() {
         //find a single data 
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
         });
@@ -39,20 +39,33 @@ async function run() {
         //delete a single product:
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await productCollection.deleteOne(query);
             res.send(result);
         })
 
+        //update a product stock
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: updatedItem
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
         //send data to the backend:
-        app.post('/product', async(req, res)=>{
+        app.post('/product', async (req, res) => {
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         })
     }
     finally {
-        
+
     }
 }
 
